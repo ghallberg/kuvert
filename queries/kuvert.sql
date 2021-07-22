deaf01f30f215960081f5bf2831313d63d873801
Gustaf Hallberg <ghallberg@gmail.com>
2021-07-23 01:26:40 +0200

    returning

--- queries/kuvert.sql
+++ queries/kuvert.sql
@@ -1,23 +1,23 @@
 -- :name fetch_open_kuvert :many
-SELECT * FROM kuvert WHERE opening_date <= date('now')
+SELECT * FROM kuvert WHERE opening_date <= date('now');
 
 -- :name fetch_recent_open_kuvert :many
-SELECT * FROM kuvert WHERE opening_date <= date('now') ORDER BY opening_date DESC LIMIT 5
+SELECT * FROM kuvert WHERE opening_date <= date('now') ORDER BY opening_date DESC LIMIT 5;
 
 -- :name fetch_open_kuvert_by_id :one
-SELECT * FROM kuvert WHERE id = :id AND opening_date <= date('now')
+SELECT * FROM kuvert WHERE id = :id AND opening_date <= date('now');
 
 -- :name fetch_kuvert_by_id :one
-SELECT title, created_at, tag, opening_date from kuvert WHERE id = :id
+SELECT title, created_at, tag, opening_date from kuvert WHERE id = :id;
 
 -- :name fetch_open_tag_kuvert :many
-SELECT * FROM kuvert WHERE opening_date <= date('now') AND tag = :tag
+SELECT * FROM kuvert WHERE opening_date <= date('now') AND tag = :tag;
 
 -- :name store_kuvert :insert
-INSERT INTO kuvert (content, opening_date, tag, title) VALUES (:content, :opening_date, :tag, :title)
+INSERT INTO kuvert (content, opening_date, tag, title) VALUES (:content, :opening_date, :tag, :title) RETURNING id AS id;
 
 -- :name fetch_all_kuvert :many
 SELECT * FROM kuvert;
 
 -- :name force_fetch_kuvert :one
-SELECT * FROM kuvert WHERE id = :id
+SELECT * FROM kuvert WHERE id = :id;
